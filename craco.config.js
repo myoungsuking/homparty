0a0b1d7a930c4fb15fcc747dc34e8682020e4b19
김명수 <myoungsu@gimmyeongsuui-MacBookPro.local>
2025-12-01 14:46:28 +0900

    Fix styled-components keyframes error with babel-plugin-styled-components and CRACO

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  babel: {
+    plugins: [
+      [
+        'babel-plugin-styled-components',
+        {
+          displayName: true,
+          fileName: false,
+        },
+      ],
+    ],
+  },
+};
+
